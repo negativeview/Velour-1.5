cb9e3b8261bdfe81089558767f87f438dd935e77
Daniel Grace <dgrace@doomstick.com>
2011-04-02 17:47:00 -0500

    This controller handles basically nothing except the home page. This one is probably MOSTLY done just because there's not a lot it's supposed to do.

---
+++ protected/DefaultController.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once('ARGTechController.php');
+
+class Default_Controller extends ARGTech_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+	
+	/**
+	 * Find most bragworthy, public information.
+	 *
+	 * This page finds the most brag-worthy activity and describes it in detail, automatically.
+	 * For simplicities sake, this must always be something public. Even if you're logged in,
+	 * this page will not show your super-awesome-but-private stuff. Brag-worthy is currently
+	 * defined by amount of activity in the last 30 days, averaged over the number of days
+	 * represented, with a minimum of five days.
+	 */
+	public function defaultAction($args)
+	{
+		require_once('classes/ActivityLog.php');
+		$this->_smarty->display('header.tpl');
+		
+		$braggables = ActivityLog::activityByObject(30);
+		$count = count($braggables);
+		
+		$public_braggables = array();
+		require_once('classes/BaseObject.php');
+		for ($i = 0; $i < $count; $i++) {
+			$bo = BaseObject::getByTypeAndId($braggables[$i]['obj_type'], $braggables[$i]['obj_id']);
+			if ($bo->isPublic())
+				$public_braggables[] = $bo;
+		}
+		
+		$this->_smarty->assign('braggables', $public_braggables);
+		$this->_smarty->display('braggables-home.tpl');
+		$this->_smarty->display('footer.tpl');
+	}
+}
