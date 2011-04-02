af0a65b50207f316aa32deff7c3052d08a782be1
Daniel Grace <dgrace@doomstick.com>
2011-04-02 17:46:17 -0500

    The very very base of my controller class. A lot is in there already, because I wrote a lot before initially commiting it. All other controllers derive from this one.

---
+++ protected/ARGTechController.php
@@ -0,0 +1,47 @@
+<?php
+
+class ARGTech_Controller
+{
+	protected $_smarty = null;
+	protected $_ownedType;
+	
+	public function __construct()
+	{
+		global $site_root;
+		
+		require_once('includes/Smarty.class.php');
+		$this->_smarty = new Smarty();
+		$this->_smarty->template_dir = $site_root . 'protected/templates/';
+		$this->_smarty->compile_dir = $site_root . 'protected/templates_c/';
+	}
+	
+	/**
+	 * Displays a list of the objects.
+	 *
+	 * Override this if you can make it better for your object. This basically displays a list
+	 * of the objects on the system. It can only really display things that are part of BaseObject,
+	 * and cannot make any assumptions about what kind of data it is.
+	 *
+	 * You can probably do better than this, honestly.
+	 */
+	
+	public function defaultAction($args)
+	{
+		require_once('classes/BaseObject.php');
+		$this->_smarty->display('header.tpl');
+
+		if (!count($args)) {
+			$this->_smarty->assign('title', $this->_ownedType['name']);
+			$res = BaseObject::getAllByType($this->_ownedType['id']);
+			
+			$this->_smarty->assign('page_number', isset($_GET['page']) ? (int)$_GET['page'] : 1);
+			$this->_smarty->assign('objects', $res);
+			$this->_smarty->assign('object_count', count($res));
+			$this->_smarty->assign('per_page', 10);
+			$this->_smarty->assign('max_pages', ceil(count($res) / 10));
+			$this->_smarty->display('object-list.tpl');
+		}
+		
+		$this->_smarty->display('footer.tpl');
+	}
+}
