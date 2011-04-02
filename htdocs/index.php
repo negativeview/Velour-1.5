032640725a1c1937749bf7280a76937e74eb953e
Daniel Grace <dgrace@doomstick.com>
2011-04-02 17:28:05 -0500

    Initial commit with htaccess and the main, simple, stupid controller.y

---
+++ htdocs/index.php
@@ -0,0 +1,60 @@
+<?
+
+require_once('core.php');
+
+if (isset($_SERVER['REDIRECT_URL'])) {
+	$request = $_SERVER['REDIRECT_URL'];
+	
+	$pieces = explode('/', $request);
+	for ($i = 0; $i < count($pieces); $i++) {
+		if (trim($pieces[$i]) == '') {
+			array_splice($pieces, $i, 1);
+			$i--;
+		}
+	}
+
+	switch(count($pieces)) {
+		// we're looking for the home page, or default controller action default
+		case 0:
+			$controller = 'default';
+			$action = 'default';
+			$args = array();
+			break;
+		default:
+			$controller = array_shift($pieces);
+			$action = 'default';
+			$args = array();
+			
+			if (count($pieces)) {
+				if (is_numeric($pieces[0])) {
+					$id = array_shift($pieces);
+					if (count($pieces))
+						$action = array_shift($pieces);
+					else
+						$action = 'default';
+				
+					$args = $pieces;
+					array_unshift($args, $id);
+				} else {
+					print_r($_SERVER);
+					die();
+					$action = $pieces[1];
+					for ($i = 2; $i < count($pieces); $i++) {
+						$args[] = $pieces[$i];
+					}
+				}
+			}
+	}
+	
+	$action = str_replace('.', '', $action);
+	
+	$controller_name = ucfirst($controller) . '_Controller';
+	
+	require_once(ucfirst($controller) . 'Controller.php');
+	$controller = new $controller_name();
+	
+	$action_name = $action . 'Action';
+	$controller->$action_name($args);
+}
+
+?>
